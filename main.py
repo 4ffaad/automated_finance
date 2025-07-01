837b2b79e73194256d5db683ef022cdaf384c9c4
Daffa <Daffafariq@gmail.com>
2025-07-01 21:41:03 +0800

    Implent payment and expenses

--- main.py
+++ main.py
@@ -8,3 +8,39 @@ st.set_page_config(
     page_title="Simple Finance App", 
     page_icon = "💰", 
     layout="wide")
+
+def load_transactions(uploaded_file):
+    try:
+        df = pd.read_csv(uploaded_file)
+        #Cleaning Data 
+            #Stripping white space in the header column
+        df.columns = [col.strip() for col in df.columns]
+        df["Amount"] = df["Amount"].str.replace(",","").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
+
+        
+        st.write(df)
+        return df 
+    except Exception as e:
+        st.error(f"Error Processing file : {str(e)}")
+        return None
+
+def main():
+    st.title("Simple Finance Dashboard")
+
+    uploaded_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
+
+    if uploaded_file is not None: 
+        df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+            tabl1, tabl2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+            with tabl1:
+                st.write(debits_df)
+            with tabl2:
+                st.write(credits_df)
+
+main()
